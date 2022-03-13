1a66c7879dd1c4810f9c85400c35281e1f2fd9a1
Ilya Gonchar <i.o.gonchar@yandex.ru>
2022-03-14 00:53:38 +0300

    fix savedmovies короткометражки

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -3,17 +3,26 @@ import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Preloader from '../Preloader/Preloader';
+import { useState } from 'react';
 
 const SavedMovies = ({ isLoggedIn, isLoading, onClickLike, onSearchSubmit, movies, isMovieSaved }) => {
+  const [isRenderShortMovies, setIsRenderShortMovies] = useState(false);
+
+  const onFilterShortMovies = (isCheckboxEnabled) => {
+    setIsRenderShortMovies(isCheckboxEnabled);
+  }
+
+  const filterShortMovies = (movies) => movies.filter((movie) => movie.duration <= 40);
+
   return (
     <>
       <Header isLoggedIn={ isLoggedIn } />
-      <SearchForm onSearchSubmit={ onSearchSubmit } />
+      <SearchForm onSearchSubmit={ onSearchSubmit } onToggleCheckbox={ onFilterShortMovies } />
       {
         isLoading && <Preloader />
       }
       {
-        !isLoading && <MoviesCardList isSavedMovies={ true } onClickLike={ onClickLike } movies={ movies } isMovieSaved={ isMovieSaved } />
+        !isLoading && <MoviesCardList isSavedMovies={ true } onClickLike={ onClickLike } movies={ isRenderShortMovies ? filterShortMovies(movies) : movies } isMovieSaved={ isMovieSaved } />
       }
       <Footer />
     </>
